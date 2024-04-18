765e4ca377954ed778b924bacdca8e1709383226
David Chiu <fdchiu@gmail.com>
2024-04-18 09:59:12 -0700

    add top menu bar component

---
+++ src/components/TopMenuBar/index.jsx
@@ -0,0 +1,22 @@
+import Logo from "components/logo"
+import HeaderMenu from "components/HeaderMenu";
+import SocialButtons from "components/SocialButtons";
+
+export default function TopMenuBar() {
+    return (
+        <div className="flex flex-col items-center w-full">
+            <div className="self-stretch">
+                <header>
+                    <div className="flex md:flex-col justify-between items-center gap-5 p-5 border-white-A700_19 border-b-[0.5px] border-solid">
+                        <div className="flex sm:flex-col justify-between items-center w-[48%] md:w-full ml-[60px] gap-5 md:ml-0">
+                            <Logo />
+                            <HeaderMenu />
+                        </div>
+                        <SocialButtons />
+                    </div>
+                </header>
+            </div>
+
+        </div>
+    );
+}
